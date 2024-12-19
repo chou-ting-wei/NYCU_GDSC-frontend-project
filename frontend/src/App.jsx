f0d4e356ab038187303738ab5e9685473ad9b7ca
ShawnFHH <160568496+ShawnFHH@users.noreply.github.com>
2024-12-19 22:20:19 +0800

    FHH

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
+import FHH from "./components/FHH";
 
 function App() {
   return (
     <Router>
+
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/FHH" element={<FHH />} />
       </Routes>
     </Router>
   );

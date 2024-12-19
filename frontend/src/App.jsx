f0414f48049246097e0f1527315d2b881a4a2014
userwei <70355885+chou-ting-wei@users.noreply.github.com>
2024-12-20 01:10:47 +0800

    Set up deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import FHH from "./components/FHH";
 import Cy407 from "./components/Cy407";
@@ -8,7 +8,6 @@ import Iris from "./components/Iris";
 function App() {
   return (
     <Router>
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/FHH" element={<FHH />} />
